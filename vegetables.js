c127587f2638c0c299f52295bcb5e842a775ca32
Pete Hamrick <pete.hamrick@gmail.com>
2021-07-06 14:20:59 -0700

    moved vegetables to their own file and import them to app.js and console log the array

---
+++ vegetables.js
@@ -0,0 +1,70 @@
+// PSEUDO CODE
+// I need to design a list that will generically refer to my vegetables
+// The list will have a title, image and a button to add it to the cart
+// The list will have a product id, name, image, description, category, and price
+
+const carrots = {
+    id: 'carrots',
+    name: 'Carrots',
+    image: './assets/carrots.jpg',
+    description: 'orange, and crunchy',
+    category: 'root',
+    price: 0.50
+};
+
+const radishes = {
+    id: 'radishes',
+    name: 'Radishes',
+    image: './assets/radishes.jpg',
+    description: 'small, spicy and crunchy',
+    category: 'root',
+    price: 0.25
+};
+
+const kale = {
+    id: 'kale',
+    name: 'Kale',
+    image: './assets/kale.jpg',
+    description: 'leafy green, nutrient rich',
+    category: 'greens',
+    price: 3.00
+};
+
+const onions = {
+    id: 'onions',
+    name: 'Onions',
+    image: './assets/onions.jpg',
+    description: 'aromatic that adds flavor to dishes when cooked',
+    category: 'aromatic',
+    price: 1.50
+};
+
+const broccoli = {
+    id: 'broccoli',
+    name: 'Broccoli',
+    image: './assets/broccoli.jpg',
+    description: 'best sauteed, roasted, or steamed. Nutritious and flavorful',
+    category: 'floret',
+    price: 2.50
+};
+
+const cabbage = {
+    id: 'cabbage',
+    name: 'Cabbage',
+    image: './assets/cabbage.jpg',
+    description: 'Green that cooks well or can be eaten raw',
+    category: 'green',
+    price: 1.25
+};
+
+const vegetables = [
+    carrots,
+    radishes,
+    kale,
+    onions,
+    broccoli,
+    cabbage
+];
+
+
+export default vegetables;
